c4c5c803140d21a0f34d943b80758075869e66ab
Anurag Munda <anurag.munda005@gmail.com>
2026-02-26 09:55:08 +0000

    add db support

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,14 @@
 import app from "./app.js";
 import "dotenv/config";
+import connectDB from "./db/index.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`),
-);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error: ", err);
+    process.exit(1);
+  });
