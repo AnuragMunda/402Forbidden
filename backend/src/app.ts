9468a5a49e86a4835b8370db0b8e4d56b633d263
Anurag Munda <anurag.munda005@gmail.com>
2026-02-28 14:40:10 +0000

    add x402 support

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,47 @@ import express, { type Express } from "express";
 import cors from "cors";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import guardianRouter from "./routes/guardian.route.js";
+import { paymentMiddleware, x402ResourceServer } from "@x402/express";
+import { HTTPFacilitatorClient } from "@x402/core/server";
+import { facilitator } from "@payai/facilitator";
+import { ExactSvmScheme } from "@x402/svm/exact/server";
 
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
+
+const adminAddress = process.env.ADMIN_ADDRESS;
+
+if (!adminAddress) {
+  console.error("Missing admin address");
+  process.exit(1);
+}
+
+const facilitatorClient = new HTTPFacilitatorClient(facilitator);
+
+app.use(
+  paymentMiddleware(
+    {
+      "POST /guardian/generateSecret": {
+        accepts: [
+          {
+            scheme: "exact",
+            price: "$0.2",
+            network: "solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1",
+            payTo: adminAddress,
+          },
+        ],
+        description: "Access to chat with guardian",
+        mimeType: "application/json",
+      },
+    },
+    new x402ResourceServer(facilitatorClient).register(
+      "solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1",
+      new ExactSvmScheme(),
+    ),
+  ),
+);
 
 app.use("/guardian", guardianRouter);
 
