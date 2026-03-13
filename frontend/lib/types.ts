5f052232c8299177562fd9adf359e73a2131706e
AnuragMunda <anurag.munda005@gmail.com>
2026-03-13 18:40:03 +0530

    add types

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -17,3 +17,45 @@ export interface ArenaCardParams {
   walletConnected: boolean;
   onOpen: Dispatch<SetStateAction<Arena | null>>;
 }
+
+export interface ArenaViewParams {
+  arena: Arena;
+  onBack: () => void;
+}
+
+export interface ArenaDetailsResponseObject {
+  arenaId: number;
+  difficulty: number;
+  hint: string;
+  category: string;
+  createdAt: Date;
+  updatedAt: Date;
+  _id: string;
+}
+
+export interface ArenaDetails {
+  arenaId: number;
+  difficulty: number;
+  hint: string;
+  category: string;
+}
+
+export interface UserHistoryResponseObject {
+  arenaId: number;
+  userAddress: Address;
+  chats: [
+    {
+      role: string;
+      parts: [{ text: string }];
+      _id: string;
+    },
+  ];
+  createdAt: Date;
+  updatedAt: Date;
+  _id: string;
+}
+
+export interface UserChats {
+  role: string;
+  content: string;
+}
