7efdc6d5755f713cd6d3686573e6e67a7871e82b
AnuragMunda <anurag.munda005@gmail.com>
2026-03-13 18:39:51 +0530

    add utilities

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,3 +1,23 @@
+import { ArenaDetailsResponseObject, UserHistoryResponseObject } from "./types";
+
 export function truncate(address: string) {
   return `${address.slice(0, 4)}…${address.slice(-4)}`;
 }
+
+export const parseArenaDetails = (arena: ArenaDetailsResponseObject) => {
+  return {
+    arenaId: arena.arenaId,
+    difficulty: arena.difficulty,
+    hint: arena.hint,
+    category: arena.category,
+  };
+};
+
+export const parseUserChats = (userHistory: UserHistoryResponseObject) => {
+  const chatHistory = userHistory.chats.map((chat) => ({
+    role: chat.role,
+    content: chat.parts[0].text,
+  }));
+
+  return chatHistory;
+};
