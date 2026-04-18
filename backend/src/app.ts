b14049f034ec8bd8d6cb97bc0f1833816e776dcc
AnuragMunda <anurag.munda005@gmail.com>
2026-04-18 19:37:20 +0530

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,10 @@ import cors from "cors";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import guardianRouter from "./routes/guardian.route.js";
 import arenaRouter from "./routes/arena.route.js";
-import { paymentMiddleware, x402ResourceServer } from "@x402/express";
+// import { paymentMiddleware, x402ResourceServer } from "@x402/express";
 import { HTTPFacilitatorClient } from "@x402/core/server";
 import { facilitator } from "@payai/facilitator";
-import { ExactSvmScheme } from "@x402/svm/exact/server";
+// import { ExactSvmScheme } from "@x402/svm/exact/server";
 
 const app: Express = express();
 
@@ -22,28 +22,31 @@ if (!adminAddress) {
 
 const facilitatorClient = new HTTPFacilitatorClient(facilitator);
 
-app.use(
-  paymentMiddleware(
-    {
-      "POST /guardian": {
-        accepts: [
-          {
-            scheme: "exact",
-            price: "$0.2",
-            network: "solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1",
-            payTo: adminAddress,
-          },
-        ],
-        description: "Access to chat with guardian",
-        mimeType: "application/json",
-      },
-    },
-    new x402ResourceServer(facilitatorClient).register(
-      "solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1",
-      new ExactSvmScheme(),
-    ),
-  ),
-);
+// app.use(
+//   paymentMiddleware(
+//     {
+//       "POST /guardian": {
+//         accepts: [
+//           {
+//             scheme: "exact",
+//             price: {
+//               amount: "2000000",
+//               asset: "D6tCeapwLY9ymPW1TaMcG6Wc83E3PtqkAjDzTNtwWnjw",
+//             },
+//             network: "solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1",
+//             payTo: adminAddress,
+//           },
+//         ],
+//         description: "Access to chat with guardian",
+//         mimeType: "application/json",
+//       },
+//     },
+//     new x402ResourceServer(facilitatorClient).register(
+//       "solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1",
+//       new ExactSvmScheme(),
+//     ),
+//   ),
+// );
 
 app.use("/guardian", guardianRouter);
 app.use("/arena", arenaRouter);
